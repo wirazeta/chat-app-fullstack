368949d875d18277d9f69c94a10b288ef51c3e07
Wira Zeta Prakosa <dontknow1213@gmail.com>
2024-02-16 18:57:13 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+<h1>About This Project</h1>
+<p>This project originates from [text](https://github.com/piyush-eon/mern-chat-app)</p>
+<p>I recreate the project using another framework, NestJS(backend) & Vite(frontend)</p>
---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+declare const module: any;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
