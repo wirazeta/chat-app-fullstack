0e552d818c4537dd9eac37a2b8251958eac3732b
wirazettaprakosa <wirazettaprakosa@gmail.com>
2024-07-16 08:54:31 +0000

    [Feature]: add Socket.io

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
 
   const port = 3000;
 
-  await app.listen(port);
+  const server =await app.listen(port);
 
   if (module.hot) {
     module.hot.accept();
@@ -45,6 +45,48 @@ async function bootstrap() {
   }
 
   console.log(`http://localhost:${port}/api`);
+
+  const io = require("socket.io")(server, {
+    pingTimeout: 60000,
+    cors:{
+      origin: "http://localhost:3000"
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log("Connected to socket.io");
+    let userData:any = '';
+    socket.on("setup", (userData) => {
+      socket.join(userData._id);
+      userData = userData;
+      socket.emit("connected");
+    });
+
+    socket.on("join chat", (room) => {
+      socket.join(room);
+      console.log("User joined room :"+room);
+    });
+
+    socket.on("typing", (room) => {socket.in(room).emit("typing")});
+    socket.on("stop typing", (room) => {socket.in(room).emit("stop typing")});
+
+    socket.on("new message", (newMessageRecieved) => {
+      var chat = newMessageRecieved.chat;
+
+      if(!chat.users) return console.log("chat.users not defined");
+
+      chat.users.forEach((user) => {
+        if(user._id == newMessageRecieved.sender._id) return;
+
+        socket.in(user._id).emit("message recieved", newMessageRecieved);
+      });
+    });
+    
+    socket.off("setup", () => {
+      console.log("User Disconnected");
+      socket.leave(userData._id);
+    });
+  });
 
 }
 bootstrap();
